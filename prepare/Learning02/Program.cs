b4ee2ae9ca25836d9f3813d08349e52863637e33
vladimirtheserious <syc23001@byui.edu>
2023-05-06 17:27:19 +0300

    new things

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "SoftWare Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job1.Display();
+        job2.Display();
+
+        Resume myResume = new Resume();
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume._name = "Vladimir Sychugov";
+
+        myResume.Display();
     }
+
 }
