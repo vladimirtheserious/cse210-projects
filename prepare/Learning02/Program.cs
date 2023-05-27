9bec9fe7a7805a78b0bbcee30e53cad8f73e3863
Vladimir <vovyasychugov@gmail.com>
2023-05-27 22:27:59 +0300

    prove w6

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,32 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Job job1 = new Job();
+        // Job job1 = new Job();
 
-        job1._company = "Apple";
-        job1._jobTitle = "Software Engeneer";
-        job1._startYear = 2020;
-        job1._endYear = 2022;
+        // job1._company = "Apple";
+        // job1._jobTitle = "Software Engeneer";
+        // job1._startYear = 2020;
+        // job1._endYear = 2022;
 
-        Job job2 = new Job();
+        // Job job2 = new Job();
 
-        job2._company = "Microsoft";
-        job2._jobTitle = "SoftWare Engeneer";
-        job2._startYear = 2022;
-        job2._endYear = 2023;
+        // job2._company = "Microsoft";
+        // job2._jobTitle = "SoftWare Engeneer";
+        // job2._startYear = 2022;
+        // job2._endYear = 2023;
 
-        job1.Display();
-        job2.Display();
+        // job1.Display();
+        // job2.Display();
 
-        Resume myResume = new Resume();
+        // Resume myResume = new Resume();
 
-        myResume._name = "Vladimir Sychugov";
+        // myResume._name = "Vladimir Sychugov";
 
-        myResume._jobs.Add(job1);
-        myResume._jobs.Add(job2);
+        // myResume._jobs.Add(job1);
+        // myResume._jobs.Add(job2);
         
-        myResume.Display();
+        // myResume.Display();
 
+        string a = Console.ReadLine();
+
+        switch(a)
+        {
+            case "+":
+                int number1 = int.Parse(Console.ReadLine());
+                int number2 = int.Parse(Console.ReadLine());
+                Console.WriteLine(number1 + number2);
+                Console.WriteLine("Это было сложение");
+                break;
+            case "-":
+                int number3 = int.Parse(Console.ReadLine());
+                int number4 = int.Parse(Console.ReadLine());
+                Console.WriteLine(number3 - number4);
+                Console.WriteLine("Это было вычитание");
+                break;
+        }   case "/":
+                int number5 = int.Parse(Console.ReadLine());
+                int number6 = int.Parse(Console.ReadLine());
+                Console.WriteLine(number5 / number6);
+                Console.WriteLine("Это было деление");
+                break;
+            case "*":
+                int number7 = int.Parse(Console.ReadLine());
+                int number8 = int.Parse(Console.ReadLine());
+                Console.WriteLine(number7 / number8);
+                Console.WriteLine("Это было деление");
+                break;
 
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,133 @@
 using System;
-
-class Program
+using System.Collections.Generic;
+namespace ScriptureProgram
 {
-    static void Main(string[] args)
+    public class Word
     {
-        Console.WriteLine("Hello Develop03 World!");
+        public string Text { get; }
+        public bool Hidden { get; private set; }
+        public Word(string text)
+        {
+            Text = text;
+            Hidden = false;
+        }
+        public void Hide()
+        {
+            Hidden = true;
+        }
+        public override string ToString()
+        {
+            return Hidden ? "______" : Text;
+        }
+    }
+    public class ScriptureReference
+    {
+        private string Book { get; }
+        public int Chapter { get; }
+        public int StartVerse { get; }
+        public int? EndVerse { get; }
+        public ScriptureReference(string book, int chapter, int startVerse, int? endVerse = null)
+        {
+            Book = book;
+            Chapter = chapter;
+            StartVerse = startVerse;
+            EndVerse = endVerse;
+        }
+        public override string ToString()
+        {
+            if (EndVerse.HasValue)
+            {
+                return $"{Book} {Chapter}:{StartVerse}-{EndVerse}";
+            }
+            else
+            {
+                return $"{Book} {Chapter}:{StartVerse}";
+            }
+        }
+    }
+    public class Scripture
+    {
+        private readonly List<Word> words;
+        public ScriptureReference Reference { get; }
+        public IReadOnlyList<Word> Words => words;
+        public Scripture(ScriptureReference reference, string text)
+        {
+            Reference = reference;
+            words = new List<Word>();
+            foreach (string word in text.Split(' '))
+            {
+                words.Add(new Word(word));
+            }
+        }
+        public bool HideRandomWord()
+        {
+            List<Word> unhiddenWords = new List<Word>();
+            foreach (Word word in words)
+            {
+                if (!word.Hidden)
+                {
+                    unhiddenWords.Add(word);
+                }
+            }
+            if (unhiddenWords.Count == 0)
+            {
+                return false;
+            }
+            Random random = new Random();
+            int randomIndex = random.Next(unhiddenWords.Count);
+            unhiddenWords[randomIndex].Hide();
+            return true;
+        }
+        public void Display()
+        {
+            Console.Write($"{Reference}\n");
+            foreach (Word word in words)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine("\n");
+        }
+        public bool AllWordsHidden()
+        {
+            foreach (Word word in words)
+            {
+                if (!word.Hidden)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    public class Program
+    {
+        private static void ClearConsole()
+        {
+            Console.Clear();
+        }
+        private static void Main()
+        {
+            Scripture scripture = new Scripture(new ScriptureReference("John", 3, 16),
+                "For God so loved the world that he gave his only Son, " +
+                "so that everyone who believes in him may not perish but may have eternal life.");
+            while (!scripture.AllWordsHidden())
+            {
+                Console.WriteLine("Press Enter to reveal the hidden words or type 'quit' to exit: ");
+                string input = Console.ReadLine();
+                if (input == "quit")
+                {
+                    break;
+                }
+                if (scripture.HideRandomWord())
+                {
+                    ClearConsole();
+                    scripture.Display();
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
     }
 }
